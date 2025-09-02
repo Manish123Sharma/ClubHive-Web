1c9e3eef4f16dd711470c7cc059e1ee036ce5e64
Manish <manish2003sharma@gmail.com>
2025-09-03 00:30:39 +0530

    server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const profileRoutes = require('./routes/profileRoutes');
+const eventRoutes = require('./routes/eventRoutes');
 
 dotenv.config();
 require('./db/connect');
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
+app.use('/event', eventRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
