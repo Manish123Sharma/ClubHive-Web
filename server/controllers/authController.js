302748cfa333666727e20d620845b4923360aa8c
Manish <manish2003sharma@gmail.com>
2025-09-02 11:50:01 +0530

    controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,8 @@
+const User = require('../models/User.js');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d"
+    });
+};
