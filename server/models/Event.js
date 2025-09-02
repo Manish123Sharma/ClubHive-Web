38e40a7128b7de6c1a6b1a11d26626e68d112ca4
Manish <manish2003sharma@gmail.com>
2025-09-02 13:59:17 +0530

    models

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,2 +1,36 @@
 const mongoose = require('../db/connect');
 const { Schema, SchemaTypes } = mongoose;
+const { v4: uuidv4 } = require('uuid');
+
+const EventSchema = new Schema({
+    event_id: {
+        type: SchemaTypes.String,
+        unique: true,
+        index: true
+    },
+    name: {
+        type: SchemaTypes.String,
+        required: true
+    },
+    category: {
+        type: SchemaTypes.String,
+        required: true
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+EventSchema.pre('save', async function (next) {
+    // Generate unique event_id if not present
+    if (!this.event_id) {
+        this.event_id = uuidv4();
+    }
+
+    next();
+});
+
+const Event = mongoose.model('Events', EventSchema);
+
+module.exports = Event;
