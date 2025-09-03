649d01378498171e0e895a52b5dc85a9dae60368
Manish <manish2003sharma@gmail.com>
2025-09-03 21:44:34 +0530

    controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -185,3 +185,21 @@ exports.updatePasswordAdmin = async (req, res) => {
         });
     }
 };
+
+exports.getAll = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.getAllAdmin = async (req, res) => {
+    try {
+        const admins = await Admin.find();
+        res.status(200).json(admins);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
