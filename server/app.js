6638326b4f144829b9952b47833c7adddc0e16d8
Manish <manish2003sharma@gmail.com>
2025-09-04 15:06:26 +0530

    server

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use('/profile', profileRoutes);
 app.use('/event', eventRoutes);
 app.use('/geo', geoRoutes);
 
-require('./services/geoServices');
+// require('./services/geoServices');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
