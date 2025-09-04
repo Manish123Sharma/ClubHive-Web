826a2374fe91976e285d05e041f28091583c38ad
Manish <manish2003sharma@gmail.com>
2025-09-04 11:01:20 +0530

    models

---
+++ server/models/Country.js
@@ -0,0 +1,45 @@
+const mongoose = require('../db/connect');
+const { Schema, SchemaTypes } = mongoose;
+const { v4: uuidv4 } = require('uuid');
+
+const CountrySchema = new Schema({
+    country_id: {
+        type: SchemaTypes.String,
+        unique: true,
+        index: true
+    },
+    countryCode: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    population: {
+        type: Number
+    },
+    areaInSqKm: {
+        type: Number
+    },
+    continent: {
+        type: String
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+CountrySchema.pre('save', async function (next) {
+    if (!this.country_id) {
+        this.country_id = uuidv4();
+    }
+
+    next();
+});
+
+const Country = mongoose.model('Country', CountrySchema);
+
+module.exports = Country;
