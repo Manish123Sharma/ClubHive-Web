e9835f1d9b4c8b43ca61916fa8401edebb34acdf
Manish <manish2003sharma@gmail.com>
2025-09-04 22:14:57 +0530

    pages

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+
+    const[fullName, setfullName] = useState('');
+    const[password, setPassword] = useState('');
+    const[email, setEmail] = useState('');
+    const[city, setCity] = useState('');
+    const[state, setState] = useState('');
+    const[gender, setGender] = useState('');
+    const[phoneNumber, setPhoneNumber] = useState('');
+    const[primarySport, setPrimarySport] = useState('');
+    const[dob, setDOB] = useState('')
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Register;
