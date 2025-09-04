f0a4b88fa4bddde80468b70f370c03af3271c5d3
Manish <manish2003sharma@gmail.com>
2025-09-04 17:39:56 +0530

    routes

--- server/routes/geoRoutes.js
+++ server/routes/geoRoutes.js
@@ -5,4 +5,14 @@ const geoController = require('../controllers/geoController');
 
 router.get('/update/all', geoController.updateAll);
 
+router.get('/getAllCountry', geoController.getAllCountry);
+
+router.get('/getAllState', geoController.getAllState);
+
+router.get('/getAllCity', geoController.getAllCity);
+
+router.get('/getStatebyCountry', geoController.getStatebyCountry);
+
+router.get('/getCitybyState', geoController.getCitybyState);
+
 module.exports = router;
