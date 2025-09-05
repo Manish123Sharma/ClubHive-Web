3d95a6e6f067b4a22c5969a2a497787ae4d3c81f
Manish <manish2003sharma@gmail.com>
2025-09-05 13:29:23 +0530

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import './App.css'
+import Login from './pages/Login';
 
 
 const App = () => {
@@ -12,7 +13,8 @@ const App = () => {
             <ToastContainer />
             <Router>
                 <Routes>
-                    <Route path='/' element={<Register />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/' element={<Login />} />
                 </Routes>
             </Router>
         </>
