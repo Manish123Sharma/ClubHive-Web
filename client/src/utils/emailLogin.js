2f458a499a0abf023292d1b735da07944a746e3f
Manish <manish2003sharma@gmail.com>
2025-09-05 13:29:55 +0530

    utils

--- client/src/utils/emailLogin.js
+++ client/src/utils/emailLogin.js
@@ -1,7 +1,25 @@
 import { toast } from "react-toastify";
 
-const emailLogin = async () =>{
-    toast.success('Login Successful');
+const emailLogin = async ({
+    email,
+    password,
+    setLoading,
+    resetFields
+}) => {
+    // toast.success('Login Successful');
+    if (!email || !password) {
+        toast.error('Please fill all the fields..');
+        setLoading(false);
+        return;
+    }
+    try {
+        toast.success('Login Successful');
+        resetFields();
+    } catch (error) {
+        console.error("Signup error", error);
+        toast.error(error.message);
+        setLoading(false);
+    }
 };
 
 export default emailLogin;
