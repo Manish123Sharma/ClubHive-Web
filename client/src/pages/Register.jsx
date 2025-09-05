34dff2becb44ee1f87850155e6512c295f49c6be
Manish <manish2003sharma@gmail.com>
2025-09-05 12:23:57 +0530

    pages

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,75 @@
 import React, { useState } from 'react';
 import './styles/Register.css';
+import { FaFacebookF, FaGoogle, FaGithub } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import emailRegister from '../utils/emailRegister';
 
 const Register = () => {
 
     const [fullName, setfullName] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
-    // const[city, setCity] = useState('');
-    // const[state, setState] = useState('');
-    // const[country, setCountry] = useState('');
-    // const[gender, setGender] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [country, setCountry] = useState('');
+    const [gender, setGender] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [primarySport, setPrimarySport] = useState('');
-    const [dob, setDOB] = useState('')
+    const [dob, setDOB] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleName = (e) => {
+        setfullName(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handlePhone = (e) => {
+        setPhoneNumber(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const handlePrimary = (e) => {
+        setPrimarySport(e.target.value);
+    };
+
+    const handleDOB = (e) => {
+        setDOB(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const handleCountry = (e) => {
+        setCountry(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const handleState = (e) => {
+        setState(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const handleCity = (e) => {
+        setCity(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const handleGender = (e) => {
+        setGender(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        emailRegister();
+    };
 
     return (
         <div className="signup-container">
@@ -31,23 +88,70 @@ const Register = () => {
                 <div className="shape shape-2"></div>
 
                 <div className="signup-card">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         {/* <div className="form-row"> */}
-                        <input type="text" placeholder='Full Name' value={fullName} onChange={(e) => {
-                            setfullName(e.target.value);
-                        }} />
+                        <input type="text" placeholder='Full Name' value={fullName} onChange={handleName} />
                         {/* </div> */}
-                        <input type="email" placeholder='Email' value={email} onChange={(e) => {
-                            setEmail(e.target.value);
-                        }} />
-                        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        <input type="number" placeholder='Phone Number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-                        <input type="text" placeholder='Primary Sport' value={primarySport} onChange={(e) => setPrimarySport(e.target.value)} />
-                        <input type="date" value={dob} onChange={(e) => {
-                            setDOB(e.target.value);
-                            console.log(dob);
-                        }} />
+                        <input type="email" placeholder='Email' value={email} onChange={handleEmail} />
+                        <input type="password" placeholder="Password" value={password} onChange={handlePassword} />
+                        <input type="number" placeholder='Phone Number' value={phoneNumber} onChange={handlePhone} />
+                        <input type="text" placeholder='Primary Sport' value={primarySport} onChange={handlePrimary} />
+                        <input type="date" value={dob} onChange={handleDOB} />
+                        <div className="form-row">
+                            <select value={country} onChange={handleCountry}>
+                                <option value="">Select Country</option>
+                                <option value="india">India</option>
+                                <option value="usa">USA</option>
+                                <option value="uk">UK</option>
+                            </select>
+                            <select value={state} onChange={handleState}>
+                                <option value="">Select State</option>
+                                <option value="hr">Haryana</option>
+                                <option value="delhi">Delhi</option>
+                                <option value="up">Uttar Pardesh</option>
+                            </select>
+                        </div>
+                        <div className="form-row">
+                            <select value={city} onChange={handleCity}>
+                                <option value="">Select City</option>
+                                <option value="rewari">Rewari</option>
+                                <option value="gurgaon">Gurgaon</option>
+                                <option value="noida">Noida</option>
+                            </select>
+                            <select value={gender} onChange={handleGender}>
+                                <option value="">Select Gender</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="others">Others</option>
+                            </select>
+                        </div>
+                        {loading
+                            ?
+                            <button disabled type="submit" className="signup-btn">
+                                Loading...
+                            </button>
+                            :
+                            <button type="submit" className="signup-btn">
+                                Register
+                            </button>
+                        }
+
                     </form>
+                    <div className="social-section">
+                        <p>or sign up with:</p>
+                        <div className="social-icons">
+                            <a href="#" className="social-icon facebook"><FaFacebookF /></a>
+                            <a href="#" className="social-icon google"><FaGoogle /></a>
+                            <a href="#" className="social-icon github"><FaGithub /></a>
+                        </div>
+                    </div>
+
+                    <p className="login-text">
+                        Already have an account?{" "}
+                        <Link to="#" className="link-danger">
+                            Login
+                        </Link>
+                    </p>
                 </div>
             </div>
         </div>
