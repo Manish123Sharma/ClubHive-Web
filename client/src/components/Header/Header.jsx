e86e4b5fb7d8fc1c51ae03e0e687a11081d0d377
Manish <manish2003sharma@gmail.com>
2025-09-06 00:10:50 +0530

    components

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import { FaSearch, FaGlobe } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { MdLocationOn } from "react-icons/md";
 import './Header.css';
 import logo from '../../assets/logo.png';
 
 const Header = () => {
     return (
         <div className='navbar'>
+            {/* Logo */}
             <div className="logo">
-                <a href="/home"><img src={logo} alt="ClubHive Logo" /></a>
+                <a  href="/home"><img src={logo} alt="ClubHive Logo" /></a>
             </div>
-            <div className="search-bar">
-                <FaSearch className="search-icon" />
-                <input
-                    type="text"
-                    placeholder="Search for events, interests or activities"
-                />
-            </div>
-            <div className="header-right">
-                <div className="dropdown">
-                    <FaGlobe className="globe-icon" />
-                    <span>Online</span>
+
+            {/* Search Bar */}
+            <div className="loc_search">
+                <div className="search-bar">
+                    <FaSearch className="search-icon" />
+                    <input
+                        type="text"
+                        placeholder="Search for events, interests or activities"
+                    />
+
                 </div>
+                <div className="location">
+                    <MdLocationOn className="location-icon" />
+                    <span>India</span>
+                </div>
+            </div>
+
+
+            {/* Right Section */}
+            <div className="header-right">
                 <div className="profile">
                     <img
-                        src="https://i.pravatar.cc/40" // replace with user profile img
+                        src="https://i.pravatar.cc/40"
                         alt="profile"
                     />
                 </div>
