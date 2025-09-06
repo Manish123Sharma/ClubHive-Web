b79ff02cd7059d07f9284472d4dd00af658e6deb
Manish <manish2003sharma@gmail.com>
2025-09-06 13:24:43 +0530

    components

--- client/src/components/Event Card/EventCard.jsx
+++ client/src/components/Event Card/EventCard.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './EventCard.css';
+import { FaHeart, FaShareAlt } from "react-icons/fa";
 
 const EventCard = () => {
     return (
-        <div>
+        <div className="event-card">
+            {/* Top image with icons */}
+            <div className="event-image-container">
+                <img
+                    className="event-image"
+                    src="https://www.topgear.com/sites/default/files/2025/01/1-Defender-Octa-review-2025.jpg?w=976&h=549"
+                    alt="Event"
+                />
+                <div className="event-icons">
+                    <button className="icon-btn">
+                        <FaHeart />
+                    </button>
+                    <button className="icon-btn">
+                        <FaShareAlt />
+                    </button>
+                </div>
+            </div>
 
+            {/* Details */}
+            <div className="event-details">
+                <h3 className="event-title">
+                    Sinhagad Half Marathon - Promo Run
+                </h3>
+                <p className="event-subtitle">Sep 07 | Pune</p>
+                <p className="event-price">Free</p>
+            </div>
         </div>
     );
 }
