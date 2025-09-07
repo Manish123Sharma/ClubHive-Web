cf816d7ef26978f1642212d77d12f9e0d4eb167a
Manish <manish2003sharma@gmail.com>
2025-09-07 16:17:15 +0530

    components

--- client/src/components/Event Card/EventCard.jsx
+++ client/src/components/Event Card/EventCard.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './EventCard.css';
 import { FaHeart, FaShareAlt } from "react-icons/fa";
 
-const EventCard = () => {
+const EventCard = ({
+    name,
+    pic,
+    date,
+    city,
+    price
+}) => {
+
+    const formattedDate = new Date(date).toLocaleDateString("en-US", {
+        month: "short",   
+        day: "2-digit",   
+        year: "numeric"   
+    });
+
+
     return (
-        <div className="event-card" onClick={() => {console.log('Indian Events');}}>
+        <div className="event-card" onClick={() => { console.log('Indian Events'); }}>
             {/* Top image with icons */}
             <div className="event-image-container">
                 <img
                     className="event-image"
-                    src="https://www.topgear.com/sites/default/files/2025/01/1-Defender-Octa-review-2025.jpg?w=976&h=549"
+                    src={pic}
                     alt="Event"
                 />
                 <div className="event-icons">
@@ -25,13 +40,21 @@ const EventCard = () => {
             {/* Details */}
             <div className="event-details">
                 <h3 className="event-title">
-                    Sinhagad Half Marathon - Promo Run
+                    {name}
                 </h3>
-                <p className="event-subtitle">Sep 07 | Pune</p>
-                <p className="event-price">Free</p>
+                <p className="event-subtitle">{formattedDate} | {city}</p>
+                <p className="event-price">{price}</p>
             </div>
         </div>
     );
 }
+
+EventCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    pic: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+};
 
 export default EventCard;
