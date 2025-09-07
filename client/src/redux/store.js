b0f06a95b84a20b92c04ae844b817943e00fe4d2
Manish <manish2003sharma@gmail.com>
2025-09-07 14:16:08 +0530

    redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../redux/slices/authSlice';
+import eventReducer from '../redux/slices/eventSlics';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        events: eventReducer,
     },
 });
