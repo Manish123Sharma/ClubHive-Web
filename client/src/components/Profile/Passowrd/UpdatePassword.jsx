dc1cbf24082059bddd9b5f1f5331dc16e386a410
Manish <manish2003sharma@gmail.com>
2025-10-07 19:03:07 +0530

    components

---
+++ client/src/components/Profile/Passowrd/UpdatePassword.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './UpdatePassword.module.css';
+
+const UpdatePassword = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default UpdatePassword;
