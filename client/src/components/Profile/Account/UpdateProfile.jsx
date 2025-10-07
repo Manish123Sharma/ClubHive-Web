9a35f757aa5535968fdb5e60863db091aa725938
Manish <manish2003sharma@gmail.com>
2025-10-07 19:02:53 +0530

    components

---
+++ client/src/components/Profile/Account/UpdateProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './UpdateProfile.module.css';
+
+const UpdateProfile = () => {
+    return (
+        <div>
+            <h1>Editing</h1>
+        </div>
+    );
+}
+
+export default UpdateProfile;
